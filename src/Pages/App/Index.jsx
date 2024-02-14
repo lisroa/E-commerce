ca04ee18e4c2f814bf961cf2c9fdb255779c2de7
Lis Roa <lisgimenaroa@gmail.com>
2024-02-14 11:31:16 -0300

    fix

--- src/Pages/App/Index.jsx
+++ src/Pages/App/Index.jsx
@@ -1,45 +1,48 @@
-import { useRoutes, BrowserRouter} from 'react-router-dom';
-import {ShoppingCartProvider} from '../../Context';
-import Home from '../Home/Index';
-import MyAccount from '../MyAccount';
-import MyOrder from '../MyOrder';
-import MyOrders from '../MyOrders';
-import NotFound from '../NotFound';
-import SignIn from '../SignIn';
-import NavBar from '../../Components/NavBar';
-import CheckoutSideMenu from '../../Components/CheckoutSideMenu';
-import './App.css'
+import { useRoutes, BrowserRouter } from "react-router-dom";
+import { ShoppingCartProvider } from "../../Context";
+import Home from "../Home/Index";
+import MyAccount from "../MyAccount";
+import MyOrder from "../MyOrder";
+import MyOrders from "../MyOrders";
+import NotFound from "../NotFound";
+import SignIn from "../SignIn";
+import NavBar from "../../Components/NavBar";
+import CheckoutSideMenu from "../../Components/CheckoutSideMenu";
+import "./App.css";
 
 const AppRoutes = () => {
   let routes = useRoutes([
-      { path: "/", element: <Home/>},
-      { path: "/men's-clothing", element: <Home/>},
-      { path: "/women's-clothing", element: <Home/>},
-      { path: "/electronics", element: <Home/>},
-      { path: "/jewelery", element: <Home/>},
-      { path: "/my-account", element: <MyAccount/>},
-      { path: "/my-order", element: <MyOrder/>},
-      { path: "/my-orders", element: <MyOrders/>},
-      { path: "/my-orders/last", element: <MyOrder/>},
-      { path: "/my-orders/:id", element: <MyOrder/>},
-      { path: "/*", element: <NotFound/>},
-      { path: "/sign-in", element: <SignIn/>}
-    ])
+    { path: "/", element: <Home /> },
+    { path: "/men's-clothing", element: <Home /> },
+    { path: "/women's-clothing", element: <Home /> },
+    { path: "/electronics", element: <Home /> },
+    { path: "/jewelery", element: <Home /> },
+    {
+      /*path: "/my-account", element: <MyAccount/> */
+    },
+    { path: "/my-order", element: <MyOrder /> },
+    { path: "/my-orders", element: <MyOrders /> },
+    { path: "/my-orders/last", element: <MyOrder /> },
+    { path: "/my-orders/:id", element: <MyOrder /> },
+    { path: "/*", element: <NotFound /> },
+    {
+      /*path: "/sign-in", element: <SignIn/>*/
+    },
+  ]);
 
-   return routes
-}
+  return routes;
+};
 
 function App() {
- return (
-   <ShoppingCartProvider> 
+  return (
+    <ShoppingCartProvider>
       <BrowserRouter>
-      <AppRoutes/>
-      <NavBar/>
-      <CheckoutSideMenu/>
-    </BrowserRouter>
-   </ShoppingCartProvider>
-   
-  )
+        <AppRoutes />
+        <NavBar />
+        <CheckoutSideMenu />
+      </BrowserRouter>
+    </ShoppingCartProvider>
+  );
 }
 
-export default App
+export default App;
